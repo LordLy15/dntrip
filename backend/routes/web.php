546d1039728f857fbf763062237699e02d3c1287
LordLy15 <adnanpb15@gmail.com>
2026-07-05 16:41:30 +0700

    Add login route for Sanctum redirect

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Login route for Sanctum redirect
+Route::get('/login', function () {
+    return redirect('/');
+})->name('login');
+
 // Fallback route for SPA
 Route::get('/{any}', function () {
     return view('welcome');
